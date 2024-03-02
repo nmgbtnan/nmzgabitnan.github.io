7f3612350fa4a35c7f00e6f4e1bb644e30f9c55e
Nemuel M Gabitanan Jr <Nemuel M Gabitanan Jr>
2024-03-03 06:42:36 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function index() {
+  return (
+    // <div className="App"></div>
+    <header class="header">
+      <div><h1>kinginamo ka gago</h1></div>
+
+    </header>
+  );
+}
+
+export default index;
